b56d9d6a024379cf7d0d6d4b717d9898cc5b71f7
dimas abiyoga <dimas.abiyoga169@gmail.com>
2022-07-01 15:32:32 +0700

    feat: implement UUID for OrderID and OrderItemID

--- internal/model/orderItem.go
+++ internal/model/orderItem.go
@@ -1,9 +1,11 @@
 package model
 
+import "github.com/google/uuid"
+
 // TODO: Change ID type from int to be string to implement UUID
 // TODO: Implement model by ourselved to remove dependency to Gorm
 type OrderItem struct {
-	OrderID  *uint           `json:"order_id"`
+	OrderID  uuid.UUID       `json:"id" gorm:"type:uuid;primaryKey"`
 	MenuID   int             `json:"menu_id" gorm:"primaryKey;not null"`
 	Status   OrderItemStatus `json:"order_item_status" gorm:"type:enum('pending', 'paid', 'prepared', 'delivered');default:'pending'"`
 	Quantity int             `json:"quantity"`
