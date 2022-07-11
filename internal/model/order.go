de43dbe275c9954ccab3c85d75e2f8d398a143c5
dimas abiyoga <dimas.abiyoga169@gmail.com>
2022-07-11 13:51:03 +0700

    fix: migration issue: uuid type to be char(36)

--- internal/model/order.go
+++ internal/model/order.go
@@ -3,8 +3,8 @@ package model
 import "github.com/google/uuid"
 
 type Order struct {
-	ID            uuid.UUID   `json:"id" gorm:"type:uuid;primaryKey"`
-	UserID        *uuid.UUID  `json:"user_id"`
+	ID            uuid.UUID   `json:"id" gorm:"type:char(36);primaryKey"`
+	UserID        *uuid.UUID  `json:"user_id" gorm:"type:char(36)"`
 	OrderStatus   OrderStatus `json:"order_status" gorm:"type:enum('pending','paid','prepared','completed','canceled');default:'pending'"`
 	TotalPrice    float64     `json:"total_price" gorm:"not null;type:decimal(25,2)"`
 	TotalQuantity int         `json:"total_quantity" gorm:"not null"`
